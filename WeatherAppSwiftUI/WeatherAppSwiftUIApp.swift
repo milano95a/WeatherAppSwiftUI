9ba97ae9ed83d061cf2d4030aa00482dda7feac2
Abdurakhmon Jamoliddinov <jamoliddinovabdu@gmail.com>
2023-06-03 13:22:32 +0500

    Initial Commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Workspace (Abdurakhmon Jamoliddinov) on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
